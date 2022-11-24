703a4d892c396fd2a8256f37f170e730d6430884
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2022-11-23 18:39:23 -0600

    completed all steps of studio assignment, including bonus mission via script.js and not sure if I was supposed to, but some changes to styles.css for moving the rocket around.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,100 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+const takeOff = document.getElementById("takeoff");
+const flightStatus = document.getElementById("flightStatus");
+const shuttleBackground = document.getElementById("shuttleBackground");
+const shuttleHeight = document.getElementById("spaceShuttleHeight");
+
+const land = document.getElementById("landing");
+
+const abort = document.getElementById("missionAbort");
+
+const upClick = document.getElementById("up");
+const downClick = document.getElementById("down");
+const rightClick = document.getElementById("right");
+const leftClick = document.getElementById("left");
+const rocketPosition = document.getElementById("rocket");
+
+let height = 0;
+let topPosition=250;
+let leftPosition=250;
+
+
+takeOff.addEventListener("click", function(event) {
+    let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+    if(response){
+        flightStatus.innerHTML="Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = 'blue';
+        height=10000;
+        rocketPosition.style.top="240px";
+        shuttleHeight.innerHTML=height;
+    };
+});
+
+land.addEventListener("click", function(event){
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    flightStatus.innerHTML="The shuttle has landed.";
+    shuttleBackground.style.backgroundColor = 'green';
+    height=0;
+    shuttleHeight.innerHTML= height;
+    rocketPosition.style.top="250px";
+    rocketPosition.style.left="250px";
+});
+
+abort.addEventListener("click", function(event){
+    let response = window.confirm("Confirm that you want to abort the mission");
+    if(response){
+        flightStatus.innerHTML="Mission aborted.";
+        shuttleBackground.style.backgroundColor='green';
+        height=0;
+        shuttleHeight.innerHTML= height;
+        rocketPosition.style.top="250px";
+        rocketPosition.style.left="250px";
+    };
+});
+
+upClick.addEventListener("click", function(event){
+    
+    if(topPosition>0){
+        topPosition-=10;
+        let topPositionString = String(topPosition) + "px";
+        rocketPosition.style.top=topPositionString;
+    };
+
+    if(height<250000){
+        height += 10000;
+        shuttleHeight.innerHTML=height;
+    };
+});
+
+downClick.addEventListener("click", function(event){
+    if(topPosition<250){
+        topPosition+=10;
+        let topPositionString = String(topPosition) + "px";
+        rocketPosition.style.top=topPositionString;
+    };
+    
+    if(height>0){
+        height-=10000;
+        shuttleHeight.innerHTML=height;
+    };
+});
+
+rightClick.addEventListener("click", function(event){
+   if(leftPosition<500){
+        leftPosition+=10;
+        let leftPositionString = String(leftPosition) + "px"
+        rocketPosition.style.left=leftPositionString;
+   };
+});
+
+leftClick.addEventListener("click", function(event){
+    if(leftPosition>0){
+        leftPosition-=10;
+        let leftPositionString = String(leftPosition) + "px"
+        rocketPosition.style.left=leftPositionString;
+    };
+});
+
+})
